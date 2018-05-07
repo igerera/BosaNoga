8bf94d7608b98ab1404d0296a111676f460bcfa7
Denis Zhebov <zhebov@gmail.com>
2018-05-07 20:45:59 +0300

    Устранено замечание: 3. При нажатии на лупу должно отображаться поле поиска https://clip2net.com/s/3TWQNdh

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function hoverProfile() {
+function headerProfile() {
     let block = document.querySelector('.header-main__hidden-panel');
     let blockActive = document.querySelector('.header-main__hidden-panel_not-hidden');
     let menu = document.querySelector('.header-main__pic_profile_menu');
@@ -11,9 +11,10 @@ function hoverProfile() {
         block.classList.toggle('header-main__hidden-panel_not-hidden');
         menu.classList.toggle('header-main__pic_profile_menu_is-active');
     }
-    
+
 }
-function hoverBasket() {
+
+function headerBasket() {
     let block = document.querySelector('.header-main__hidden-panel');
     let blockActive = document.querySelector('.header-main__hidden-panel_not-hidden');
     let menu = document.querySelector('.header-main__pic_basket_menu');
@@ -26,19 +27,23 @@ function hoverBasket() {
         block.classList.toggle('header-main__hidden-panel_not-hidden');
         menu.classList.toggle('header-main__pic_basket_menu_is-active');
     }
+
+}
+
+function headerSearch() {
+    let block = document.querySelector('.header-main__search');
+    let block2 = document.querySelector('.header-main__pic_search');
+    block.classList.toggle('header-main__search_active');
+    block2.classList.toggle('header-main__pic_search_is-hidden');
     
 }
 
+//Видимость блока корзина и профиля
+let profileHeader = document.querySelector('.header-main__pic_profile');
+let basketHeader = document.querySelector('.header-main__pic_basket');
+profileHeader.onclick = headerProfile;
+basketHeader.onclick = headerBasket;
 
-function droppedMenuActive() {
-    console.log('1');
-}
-//Видимость блока корзина и профиль
-let profileHover = document.querySelector('.header-main__pic_profile');
-let basketHover = document.querySelector('.header-main__pic_basket');
-profileHover.onclick = hoverProfile;
-basketHover.onclick = hoverBasket;
-
-//Видимость выпадающего меню
-let droppedMenu = document.querySelectorAll('.main-menu__item');
-console.log(droppedMenu)
+//Видимость меню поиска
+let searchHeader = document.querySelector('.header-main__pic_search');
+searchHeader.onclick = headerSearch;
