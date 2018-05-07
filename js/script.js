e785e81793f8e654a38660ca672e12f87877af93
Denis Zhebov <zhebov@gmail.com>
2018-05-07 19:40:33 +0300

    Устранено замечение: 1. У раздвижной панели в макете есть маленькая круглая зарубка, показывающая, что было нажато: https://clip2net.com/s/3TWQLpb (см. раздел личный кабинет)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
-
-function hoverHeader() {
+function hoverProfile() {
     let block = document.querySelector('.header-main__hidden-panel');
-    block.classList.toggle('header-main__hidden-panel_not-hidden');
+    let blockActive = document.querySelector('.header-main__hidden-panel_not-hidden');
+    let menu = document.querySelector('.header-main__pic_profile_menu');
+    let menuActive = document.querySelector('.header-main__pic_profile_menu_is-active');
+    let menu2Active = document.querySelector('.header-main__pic_basket_menu_is-active');
+    if (menu2Active) {
+        menu2Active.classList.toggle('header-main__pic_basket_menu_is-active');
+        menu.classList.toggle('header-main__pic_profile_menu_is-active');
+    } else {
+        block.classList.toggle('header-main__hidden-panel_not-hidden');
+        menu.classList.toggle('header-main__pic_profile_menu_is-active');
+    }
+    
 }
+function hoverBasket() {
+    let block = document.querySelector('.header-main__hidden-panel');
+    let blockActive = document.querySelector('.header-main__hidden-panel_not-hidden');
+    let menu = document.querySelector('.header-main__pic_basket_menu');
+    let menuActive = document.querySelector('.header-main__pic_basket_menu_is-active');
+    let menu2Active = document.querySelector('.header-main__pic_profile_menu_is-active');
+    if (menu2Active) {
+        menu.classList.toggle('header-main__pic_basket_menu_is-active');
+        menu2Active.classList.toggle('header-main__pic_profile_menu_is-active');
+    } else {
+        block.classList.toggle('header-main__hidden-panel_not-hidden');
+        menu.classList.toggle('header-main__pic_basket_menu_is-active');
+    }
+    
+}
+
 
 function droppedMenuActive() {
     console.log('1');
@@ -10,13 +36,9 @@ function droppedMenuActive() {
 //Видимость блока корзина и профиль
 let profileHover = document.querySelector('.header-main__pic_profile');
 let basketHover = document.querySelector('.header-main__pic_basket');
-profileHover.onclick = hoverHeader;
-basketHover.onclick = hoverHeader;
+profileHover.onclick = hoverProfile;
+basketHover.onclick = hoverBasket;
 
 //Видимость выпадающего меню
 let droppedMenu = document.querySelectorAll('.main-menu__item');
 console.log(droppedMenu)
-
-
-
-
