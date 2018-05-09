d1438470bea12e97cf670f3b25968fc7a6ef7ff7
Denis Zhebov <zhebov@gmail.com>
2018-05-09 12:16:04 +0300

    Устранено замечание: У слайдера снизить скорость смены изображений?)) А то рябит немного в глазах. Плюс внизу у слайдера буллиты — они не меняются сейчас при смене изображения. Должны меняться. И они, и стрелки слева и справа у баннеров должны быть кликабельны - то есть тыкаешь в буллит, нужное изображение открывается, тыкаешь на стрелку, изображение перелистывается.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Функции видимости блока корзина и профиля
+//Видимость блока корзина и профиля
 function headerHiddenPanelProfileVisibility() {
     let block = document.querySelector('.header-main__hidden-panel');
     let blockActive = document.querySelector('.header-main__hidden-panel_not-hidden');
@@ -31,6 +31,11 @@ function headerHiddenPanelBasketVisibility() {
 
 }
 
+let headerProfile = document.querySelector('.header-main__pic_profile');
+let headerBasket = document.querySelector('.header-main__pic_basket');
+headerProfile.onclick = headerHiddenPanelProfileVisibility;
+headerBasket.onclick = headerHiddenPanelBasketVisibility;
+
 //Функция видимости меню поиска
 function headerMainSearchVisibility() {
     let block = document.querySelector('.header-main__search');
@@ -40,12 +45,84 @@ function headerMainSearchVisibility() {
     
 }
 
-//Видимость блока корзина и профиля
-let headerProfile = document.querySelector('.header-main__pic_profile');
-let headerBasket = document.querySelector('.header-main__pic_basket');
-headerProfile.onclick = headerHiddenPanelProfileVisibility;
-headerBasket.onclick = headerHiddenPanelBasketVisibility;
-
-//Видимость меню поиска
 let headerSearch = document.querySelector('.header-main__pic_search');
 headerSearch.onclick = headerMainSearchVisibility;
+
+//Слайдер
+
+window.requestAnimationFrame = (function () { // для поддержки requestAnimationFrame всеми браузерами
+    return window.requestAnimationFrame ||
+           function (callback) {
+               return window.setTimeout(callback, 1000 / 60);
+           };
+})();
+// функция слайдера
+function slider(f, img, button, V, Vo, arrows) {
+console.log(button.length);
+var iii = 0,
+  start = null,
+  clear = 0;
+function step(time) {
+if (start === null) start = time;
+var progress = time - start;
+if (progress > V) {
+  start = null;
+  for (var i=0; i<img.length; i++) {
+    img[i].style.zIndex = "0";
+    button[i].style.opacity = "0.5";
+  }
+  img[iii].style.zIndex = "1";
+  iii = ((iii != (img.length - 1)) ? (iii + 1) : 0);
+  img[iii].style.zIndex = "2";
+  img[iii].style.opacity = "0";
+  button[iii].style.opacity = "1";
+} else if(img[iii].style.opacity != "") {
+  img[iii].style.opacity = ((progress/Vo < 1) ? (progress/Vo) : 1);
+}
+if(clear != "0" && progress > Vo) {} else {requestAnimationFrame(step);}
+}
+requestAnimationFrame(step);
+f.onmouseenter = function() { if(clear == "0") clear = "1"; }  // при наведении на слайдер
+f.onmouseleave = function() { if(clear == "1") {clear = "0"; requestAnimationFrame(step);} }  // курсор убран со слайдера
+for (var j=0; j<button.length; j++) {  // при нажатии кнопок
+button[j].onclick = function() {
+  for (var i=0; i<img.length; i++) {
+    img[i].style.zIndex = "0";
+    button[i].style.opacity = "0.5";
+  }
+iii = +this.value;
+  img[this.value].style.zIndex = "2";
+  button[this.value].style.opacity = "1";
+  console.log(this.value);
+}
+arrows[0].onclick = function(){
+    console.log(iii); 
+    img[iii].style.zIndex = "0";
+    button[iii].style.opacity = "0.5";
+    iii--;
+    iii = ((iii < 0) ? img.length-1 : iii);
+    console.log(iii); 
+  img[iii].style.zIndex = "2";
+  button[iii].style.opacity = "1";
+}
+arrows[1].onclick = function(){
+    console.log(iii); 
+    img[iii].style.zIndex = "0";
+    button[iii].style.opacity = "0.5";
+    iii++;
+    iii = ((iii === img.length) ? 0 : iii);
+    console.log(iii); 
+  img[iii].style.zIndex = "2";
+  button[iii].style.opacity = "1";
+}
+}
+}  
+
+var f = document.querySelector('.slider__pictures'),
+a = f.getElementsByClassName('slider__image'),
+button = f.getElementsByClassName('slider__circles')[0].getElementsByClassName('slider__circle'),
+arrows = f.getElementsByClassName('slider__arrow');
+console.log(arrows);
+slider(f, a, button, '4000', '1000', arrows);
+
+
